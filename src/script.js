21dde0b8520d1c12c8a21f4607dc7cb584d3bcf0
biggieliten <filipisaksson00@gmail.com>
2026-04-17 08:43:46 +0200

    add function to render news cards

--- src/script.js
+++ src/script.js
@@ -17,7 +17,37 @@ async function renderCanImages() {
 		canElement.append(canImage);
 		carousellElement.append(canElement)
 	});
+}
 
+async function renderNewsCards() {
+	const newsListElement = document.querySelector(".news-list")
+	if (!newsListElement) return;
+
+	const response = await fetch("./assets/news.json");
+	const news = await response.json();
+
+	news.forEach(n => {
+		const listItemElement = document.createElement("li");
+		const listItemImage = document.createElement("img");
+		const listItemHeader = document.createElement("h3");
+		const headerAnchor = document.createElement("a");
+		const listItemDate = document.createElement("p");
+
+		listItemImage.src = n.path;
+		listItemImage.alt = n.id;
+		headerAnchor.textContent = n.title;
+		listItemDate.textContent = n.date
+
+		listItemElement.classList = `news-list-item ${n.id}`;
+		listItemImage.classList = `news-list-item-image ${n.id}`;
+		listItemHeader.classList = `news-list-item-header ${n.id}`;
+		listItemDate.classList = `news-list-item-date ${n.id}`;
+
+		listItemHeader.append(headerAnchor);
+		listItemElement.append(listItemImage, listItemHeader, listItemDate);
+		newsListElement.append(listItemElement);
+	});
 }
 
 await renderCanImages();
+await renderNewsCards();
